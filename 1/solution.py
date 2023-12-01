3a6db90fef72aa261cfd1842944ffacdd3721d7d
stefan.eysoldt <stefan.eysoldt@ecc.de>
2023-12-01 06:51:29 +0100

    1

---
+++ 1/solution.py
@@ -0,0 +1,58 @@
+from helpers import *
+import re
+
+config = [
+    2,
+    2,
+]
+
+
+letters = "abcdefghijklmnopqrstuvwxyz"
+
+
+def solution1():
+    s = 0
+    for e in data:
+        f = e.strip(letters)
+        g = f[0] + f[-1]
+        s = s + int(g)
+    print(s)
+
+
+def solution2():
+    s = 0
+    translate = {
+        "one": "1",
+        "two": "2",
+        "three": "3",
+        "four": "4",
+        "five": "5",
+        "six": "6",
+        "seven": "7",
+        "eight": "8",
+        "nine": "9",
+    }
+    for e in data:
+        print("##")
+        print(e)
+
+        words = "|".join(translate.keys())
+
+        start = re.search(rf"{words}|\d", e)
+        number = start.group(0)
+        if len(number) > 1:
+            number = translate[number]
+        end = re.search(rf".*({words}|\d)", e)
+        num2 = end.group(1)
+        if len(num2) > 1:
+            num2 = translate[num2]
+        s = s + int(number + num2)
+    print(s)
+
+
+if __name__ == "__main__":
+    data = load("input.txt") if config[0] else load("sample.txt")
+    if config[1] == 1:
+        solution1()
+    else:
+        solution2()
